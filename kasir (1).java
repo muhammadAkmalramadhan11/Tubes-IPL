0d07398c9b77bd69823710b2820848510fc3ce10
GiyanRa <147681978+GiyanRa@users.noreply.github.com>
2023-11-27 19:09:58 +0700

    Add files via upload

---
+++ kasir (1).java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Date;
+import java.util.StringTokenizer;
+import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.table.DefaultTableModel;
+
+class kasir extends JFrame {
+    JLabel title = new JLabel (-)
+    date tgl = new date();
+    JLabel Kodebarang = new JLabel("Kode Barang");
+
+    JLabel keranjang_jl_merk = new JLabel("Merk");
+    JTextField keranjang_jtf_merk = new JTextField();
+    JLabel keranjang_jl_hargajual = new JLabel("Harga Jual");
+    JTextField keranjang_jtf_hargajual = new JTextField();
+    JLabel keranjang_jl_jumlahitem = new JLabel("Jumlah Item");
+    JTextField keranjang_jtf_item = new JTextField();
+    JLabel keranjang_jl_jumlah = new JLabel("Jumlah");
+    JTextField keranjang_jtf_jumlah = new JTextField();
+}
