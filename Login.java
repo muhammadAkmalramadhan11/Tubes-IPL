0d3cff0a66fecaa9ed490b68de1148ea87a87545
IqbalMaulana2727 <152207265+IqbalMaulana2727@users.noreply.github.com>
2023-12-09 03:01:18 +0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -20,7 +20,31 @@ class login extends JFrame {
     }
 
     void KomponenVisual() {
+        getContentPane().setLayout(null);
+		
+		getContentPane().add(menu);
+		menu.setBounds(0, 11, 424, 90);
+		
+		getContentPane().add(username);
+		username.setBounds(87, 127, 150, 20);
+		username.setForeground(Color.white);
+		username.setFont(new Font("Rockwell", Font.BOLD, 17));
 
+		getContentPane().add(lpassword);
+		lpassword.setBounds(87, 179, 150, 20);
+		lpassword.setForeground(Color.white);
+		lpassword.setFont(new Font("Rockwell", Font.BOLD, 17));
+
+		getContentPane().add(txtusername);
+		txtusername.setBounds(187, 127, 130, 25);
+		getContentPane().add(txtpassword);
+		txtpassword.setBounds(187, 179, 130, 25);
+
+		getContentPane().add(masuk);
+		masuk.setBounds(187, 231, 130, 25);
+
+		getContentPane().setBackground(new Color(13, 55, 73));
+		setVisible(true);
     }
 
     void AksiReaksi () {
