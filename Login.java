45c2067d62e255ade80ac714f2e5049204ab1056
IqbalMaulana2727 <152207265+IqbalMaulana2727@users.noreply.github.com>
2023-12-11 01:20:22 +0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -20,7 +20,7 @@ class login extends JFrame {
     }
 
     void KomponenVisual() {
-        getContentPane().setLayout(null);
+		getContentPane().setLayout(null);
 		
 		getContentPane().add(menu);
 		menu.setBounds(0, 11, 424, 90);
@@ -45,9 +45,53 @@ class login extends JFrame {
 
 		getContentPane().setBackground(new Color(13, 55, 73));
 		setVisible(true);
-    }
+	}
 
-    void AksiReaksi () {
-        
-    }
+	void AksiReaksi() {
+		masuk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent me) {
+				try {
+					String nama = txtusername.getText();
+					String password = txtpassword.getText();
+
+					Class.forName("com.mysql.jdbc.Driver");
+
+					Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/apkapotek", "root", "");
+
+					Statement st = koneksi.createStatement();
+					String sql = "select * from admin where Username = '" + nama + "' and Password = '" + password + "'";
+
+					ResultSet rs = st.executeQuery(sql);
+					if (rs.next()) {
+						if ((rs.getString(1).equals(nama)) && (rs.getString(2).equals(password))) {
+							JOptionPane.showMessageDialog(null, "Selamat Datang " + nama, "Konfirmasi",
+									JOptionPane.INFORMATION_MESSAGE);
+						}
+						home go = new home(nama); // pemanggilan class lain
+						go.KomponenVisual();
+						go.AksiReaksi();
+						dispose(); // untuk menutup login
+					} else if(nama.equals("")||password.equals("")){
+						JOptionPane.showMessageDialog(null, "Nama atau Password Masih Kosong", "Konfirmasi",
+								JOptionPane.INFORMATION_MESSAGE);
+					} else{
+						JOptionPane.showMessageDialog(null, "Username atau Password Salah", "Konfirmasi",
+								JOptionPane.INFORMATION_MESSAGE);
+					}
+					st.close();
+					koneksi.close();
+					txtusername.setText("");
+					txtpassword.setText("");
+				} catch (Exception ex) {
+					System.out.println("Error: " + ex);
+				}
+			}
+		});
+
+	}
+
+	public static void main(String[] args) {
+		new login();
+		
+	}
 }
